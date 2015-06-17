214b7e98fcd1f85aaa6dc785b46173e5a6c47063
Brian Morgan <brian.s.morgan@gmail.com>
2015-06-17 17:08:36 -0400

    pass in file context to replacement function
    pass the file object as the last argument when calling the replacement function.

--- index.js
+++ index.js
@@ -4,27 +4,46 @@ var through = require('through2');
 var rs = require('replacestream');
 var istextorbinary = require('istextorbinary');
 
+function wrapWithFile(func, file) {
+    if (typeof func != 'function')
+        return func;
+
+    var new_func = function() {
+        var args = [];
+        for (var i=0; i<arguments.length; i++) {
+            args[i] = arguments[i];
+        }
+        args.push(file);
+        return func.apply(this, args);
+    };
+
+    return new_func;
+}
+
+
 module.exports = function(search, replacement, options) {
   var doReplace = function(file, enc, callback) {
+    var repl = wrapWithFile(replacement, file);
+
     if (file.isNull()) {
       return callback(null, file);
     }
 
     function doReplace() {
       if (file.isStream()) {
-        file.contents = file.contents.pipe(rs(search, replacement));
+        file.contents = file.contents.pipe(rs(search, repl));
         return callback(null, file);
       }
 
       if (file.isBuffer()) {
         if (search instanceof RegExp) {
-          file.contents = new Buffer(String(file.contents).replace(search, replacement));
+          file.contents = new Buffer(String(file.contents).replace(search, repl));
         }
         else {
           var chunks = String(file.contents).split(search);
 
           var result;
-          if (typeof replacement === 'function') {
+          if (typeof repl === 'function') {
             // Start with the first chunk already in the result
             // Replacements will be added thereafter
             // This is done to avoid checking the value of i in the loop
@@ -33,7 +52,7 @@ module.exports = function(search, replacement, options) {
             // The replacement function should be called once for each match
             for (var i = 1; i < chunks.length; i++) {
               // Add the replacement value
-              result.push(replacement(search));
+              result.push(repl(search, file));
 
               // Add the next chunk
               result.push(chunks[i]);
@@ -42,7 +61,7 @@ module.exports = function(search, replacement, options) {
             result = result.join('');
           }
           else {
-            result = chunks.join(replacement);
+            result = chunks.join(repl);
           }
 
           file.contents = new Buffer(result);
@@ -58,7 +77,7 @@ module.exports = function(search, replacement, options) {
         if (err) {
           return callback(err, file);
         }
-        
+
         if (!result) {
           callback(null, file);
         } else {
@@ -67,7 +86,7 @@ module.exports = function(search, replacement, options) {
       });
 
       return;
-    } 
+    }
 
     doReplace();
   };
